7771772fe86028368a99a2ce940ae1d0dd64ab95
982956727 <982956727@qq.com>
2023-12-09 11:42:18 +0800

    deploy

---
+++ precache-manifest.6ff3bfe9fa6d2b4b54a2be4fedd853b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e25c6c5d1b1e09d685a",
+    "url": "/keep-1-website/css/app.5dbbcfb5.css"
+  },
+  {
+    "revision": "b7496aef372a0052e1f42725b9ee7b07",
+    "url": "/keep-1-website/index.html"
+  },
+  {
+    "revision": "2e25c6c5d1b1e09d685a",
+    "url": "/keep-1-website/js/app.0f52ef42.js"
+  },
+  {
+    "revision": "88ca6d50685325067f83",
+    "url": "/keep-1-website/js/chunk-vendors.17d95792.js"
+  },
+  {
+    "revision": "f35f2a953506b9bda569d938bc2ecf19",
+    "url": "/keep-1-website/manifest.json"
+  },
+  {
+    "revision": "819199e8ce4b03107872911b13a129aa",
+    "url": "/keep-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-1-website/robots.txt"
+  }
+]);
